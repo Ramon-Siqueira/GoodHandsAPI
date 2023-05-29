0d1b8561f041959bebe1e6f421b6d546858ae075
unknown <campos.phc1@gmail.com>
2023-05-29 10:06:22 -0300

    Arrumando branch

--- GoodHandsAPI/src/main/java/com/github/ProjetoGoodHands/GoodHands/repository/UsuarioRepository.java
+++ GoodHandsAPI/src/main/java/com/github/ProjetoGoodHands/GoodHands/repository/UsuarioRepository.java
@@ -1,12 +1,13 @@
 package com.github.ProjetoGoodHands.GoodHands.repository;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 import com.github.ProjetoGoodHands.GoodHands.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-	public List<Usuario> findAllByUsuarioContainingIgnoreCase(@Param("usuario") String usuario);
+
+	public Optional<Usuario> findByUsuario(String usuario);
+
 }
