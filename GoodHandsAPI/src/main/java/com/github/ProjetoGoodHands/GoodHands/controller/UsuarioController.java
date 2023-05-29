c0dcd17518a0748620a86c3cf2826af315b764cc
kaiquesilvadev <j.kaique1570@gmail.com>
2023-05-29 15:40:21 -0300

    atualização da classe usuário, criação da classe service

--- GoodHandsAPI/src/main/java/com/github/ProjetoGoodHands/GoodHands/controller/UsuarioController.java
+++ GoodHandsAPI/src/main/java/com/github/ProjetoGoodHands/GoodHands/controller/UsuarioController.java
@@ -7,19 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.github.ProjetoGoodHands.GoodHands.model.Usuario;
+import com.github.ProjetoGoodHands.GoodHands.model.UsuarioLogin;
 import com.github.ProjetoGoodHands.GoodHands.repository.UsuarioRepository;
+import com.github.ProjetoGoodHands.GoodHands.service.UsuarioService;
 
 import jakarta.validation.Valid;
 
@@ -29,46 +28,47 @@ import jakarta.validation.Valid;
 public class UsuarioController {
 
 	@Autowired
+	private UsuarioService usuarioService;
+
+	@Autowired
 	private UsuarioRepository usuarioRepository;
 
-	@GetMapping
+	@GetMapping("/all")
 	public ResponseEntity<List<Usuario>> getAll() {
+
 		return ResponseEntity.ok(usuarioRepository.findAll());
+
 	}
 
-	// Buscar por Id
 	@GetMapping("/{id}")
 	public ResponseEntity<Usuario> getById(@PathVariable Long id) {
 		return usuarioRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+	}
+
+	@PostMapping("/logar")
+	public ResponseEntity<UsuarioLogin> autenticarUsuario(@RequestBody Optional<UsuarioLogin> usuarioLogin) {
+
+		return usuarioService.autenticarUsuario(usuarioLogin)
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+
+	@PostMapping("/cadastrar")
+	public ResponseEntity<Usuario> postUsuario(@RequestBody @Valid Usuario usuario) {
+
+		return usuarioService.cadastrarUsuario(usuario)
+				.map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+
+	}
+
+	@PutMapping("/atualizar")
+	public ResponseEntity<Usuario> putUsuario(@Valid @RequestBody Usuario usuario) {
+
+		return usuarioService.atualizarUsuario(usuario)
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-	}
-
-	@GetMapping("/usuarios/{titulo}")
-	public ResponseEntity<Optional<Usuario>> getByTitulo(@PathVariable String usuario) {
-		return ResponseEntity.ok(usuarioRepository.findByUsuario(usuario));
-	}
-
-	@PostMapping
-	public ResponseEntity<Usuario> post(@Valid @RequestBody Usuario usuario) {
-		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(usuario));
-	}
-
-	@PutMapping
-	public ResponseEntity<Usuario> put(@Valid @RequestBody Usuario usuario) {
-		return usuarioRepository.findById(usuario.getId())
-				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.save(usuario)))
-				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-	}
-
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	@DeleteMapping("{id}")
-	public void delete(@PathVariable Long id) {
-		Optional<Usuario> postagem = usuarioRepository.findById(id);
-
-		if (postagem.isEmpty())
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-
-		usuarioRepository.deleteById(id);
 
 	}
 
